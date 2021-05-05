a3b4e237482aba5b6508a692db189800d04466e6
Luan-vP <luanvanpletsen@gmail.com>
2021-05-05 22:40:35 +0100

    Implement update_q_value

--- test.py
+++ test.py
@@ -11,3 +11,8 @@ print(test_player.get_q_value((1,1,1,1), (0,1)) == 0)
 # Test assigned pair
 test_player.q[(2,2,2,2),(1,1)] = 5
 print(test_player.get_q_value((2,2,2,2),(1,1)) == 5)
+
+# Test update_q
+
+test_player.update_q_value([2,2,2,2], (1,1), 5, 1, 3)
+print(test_player.q[(2,2,2,2),(1,1)] == 4.5) 
